246dc2082aa7f30ea1eaa0289bf1c8b2a7488f34
yahellx8 <yahelbl1000@gmail.com>
2026-01-05 18:48:44 +0200

    x

--- enemy.py
+++ enemy.py
@@ -6,9 +6,11 @@ class Enemy:
     time_to_change_direction = 0
 
     def __init__(self):
-        pick_new_direction = random.randint(1, 3)
+        direction_options = [(0, 1), (0, -1), (1, 0), (-1, 0), (0, 0)]
+        pick_new_direction = direction_options[random.randint(0, len(direction_options) - 1)]
         self.direction = pick_new_direction
-        print(pick_new_direction)
+        self.time_to_change_direction = random.uniform(0.3, 1.0)
+        print(pick_new_direction) #debug
 
 
-Enemy()
+enemy1 = Enemy()
